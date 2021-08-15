ce8025036c077b8d6ed1661c94de91ca3ab2d8a1
Ettore Aquino <ettore@ettoreaquino.com>
2021-08-15 11:38:10 -0300

    Refactoring PowerSystem class to simplify parameters

--- powersddp/core/system.py
+++ powersddp/core/system.py
@@ -1,27 +1,32 @@
 """Module to handle classes and methods related to a selected Power System.
-This module should follow a systems.json file standar:
-{
-  "{name}": {
-    "shedding_cost": float,
-    "load": [float, float, float],
-    "n_disc": int,
-    "n_est": int,
-    "n_cen": int,
-    "generation_units": [
-      {"type": "hydro",
-       "name": "str",
-       "v_max": float,
-       "v_min": float,
-       "prod": float,
-       "flow_max": float,
-       "inflow_scenarios":[<list>]},
-      {"type": "thermal", "name": "str", "capacity": "float", "cost": float},
-      ...
-    ]
-  }
-}
-Where {name} should be changed to whatever name you may choose to your system.
-For example, 'Test01'. Check README.md file.
+This module should follow a systems.yml file standard:
+
+# system.yml
+load: [float,float,float]
+discretizations: int
+stages: int
+scenarios: int
+outage_cost: float
+hydro-units: !include system-hydro.yml
+thermal-units: !include system-thermal.yml
+
+# system-hydro.yml
+-
+  name: str
+  v_max: float
+  v_min: float
+  prod: float
+  flow_max: float
+  inflow_scenarios:
+    - list<float>
+    - list<float>
+    - list<float>
+
+# system-thermal.yml
+-
+  name: str
+  capacity: float
+  cost: float
 """
 
 from abc import ABC, abstractclassmethod
@@ -33,27 +38,41 @@ YmlLoader.add_constructor("!include", YmlLoader.include)
 
 
 class PowerSystemInterface(ABC):
+    # Abstract Class to Power System.
+
     @abstractclassmethod
     def load_system(self):
         raise NotImplementedError
 
 
 class PowerSystem(PowerSystemInterface):
-    def __init__(self, verbose: bool = False, **kwargs):
-        self.__verbose = verbose
-        self.__dict__.update(kwargs)
-        self.load_system()
+    """Singleton Class to instantiate a Power System.
 
-    def load_system(self):
-        if "path" in self.__dict__:
-            with open(self.path, "r") as f:
-                data = yaml.load(f, YmlLoader)
+    A Power System is defined based on set of parameters, including the systems parameters
+    and all the generation units. Both thermal and hydro.
 
-                self.data = data
-                if self.__verbose:
-                    print("System loaded from {} file".format(self.path))
-        elif "data" in self.__dict__:
-            if self.__verbose:
-                print("System loaded from 'data' payload")
+    Note: Either initialize a Power System by providing the path to a systems.yml file or
+    by providing a dictionary containing all the necessary data.
+
+    Attributes
+    ----------
+    path : str, optional
+        Path to the systems.yml file
+    data : dict, optional
+        Dictionary containing all of the .
+
+    """
+
+    def __init__(self, path: str = None, data: dict = None):
+        self.data = self.load_system(path=path, data=data)
+
+    def load_system(self, *, path: str = None, data: dict = None):
+        if path:
+            with open(path, "r") as f:
+                return yaml.load(f, YmlLoader)
+        elif data:
+            return data
         else:
-            raise NotImplementedError
+            raise ValueError(
+                "load_system() should receive path=str or data=dict as arguments"
+            )
